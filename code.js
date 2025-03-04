56a85410f5b3c490d572ad1faf4f6c4f69f245ce
mjschrader307 <mjschrader307@gmail.com>
2025-03-03 17:19:02 -0700

    Not returning array

--- code.js
+++ code.js
@@ -1,8 +1,17 @@
-function fib(n) {
-  // Base cases
-  if (n < 0) return 0;
-  if (n == 1) return 1;
-  if (n == 0) return 0;
+function computeFib(n) {
+    // Base cases
+    if (n <= 0) return 0;
+    if (n == 1) return 1;
 
-  return fib(n - 2) + fib(n - 1);
+    return computeFib(n - 2) + computeFib(n - 1);
+}
+
+function fib(n) {
+  let return_arr = [];
+
+  for (let i = 0; i < n + 1; i++) {
+    return_arr.push(computeFib(i));
+  }
+
+  return fib(n);
 }
